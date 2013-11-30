b3e3a6c8119a9019ee94309b07bcc239f783f090
Cody Creager <pi.rubiks@gmail.com>
2013-11-29 20:32:36 -0500

    Updated to 2.0.

--- src/antichamber.c
+++ src/antichamber.c
@@ -1,123 +1,119 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
 #define DATE 1
 
-#define MY_UUID { 0x17, 0x90, 0xF9, 0xFE, 0x61, 0x71, 0x4C, 0xE7, 0x8F, 0xCB, 0xC8, 0x30, 0x32, 0x6A, 0xCF, 0xA4 }
-PBL_APP_INFO(MY_UUID,
-			"Antichamber", "Computing Eureka",
-			1, 0, /* App version */
-			DEFAULT_MENU_ICON,
-			APP_INFO_WATCH_FACE);
+Window* window;
+GBitmap* bs[12];
+GBitmap* bcolon;
+GBitmap* bdash;
+BitmapLayer* bl[10];
+uint32_t bsh[12] = {RESOURCE_ID_IMAGE_0, RESOURCE_ID_IMAGE_1, RESOURCE_ID_IMAGE_2, RESOURCE_ID_IMAGE_3, RESOURCE_ID_IMAGE_4, RESOURCE_ID_IMAGE_5, RESOURCE_ID_IMAGE_6, RESOURCE_ID_IMAGE_7, RESOURCE_ID_IMAGE_8, RESOURCE_ID_IMAGE_9, RESOURCE_ID_IMAGE_colon, RESOURCE_ID_IMAGE_dash};
 
-Window window;
-HeapBitmap bs[12];
-HeapBitmap* bcolon;
-HeapBitmap* bdash;
-BitmapLayer bl[10];
-int bsh[12] = {RESOURCE_ID_IMAGE_0, RESOURCE_ID_IMAGE_1, RESOURCE_ID_IMAGE_2, RESOURCE_ID_IMAGE_3, RESOURCE_ID_IMAGE_4, RESOURCE_ID_IMAGE_5, RESOURCE_ID_IMAGE_6, RESOURCE_ID_IMAGE_7, RESOURCE_ID_IMAGE_8, RESOURCE_ID_IMAGE_9, RESOURCE_ID_IMAGE_colon, RESOURCE_ID_IMAGE_dash};
-
-void setSegs(BitmapLayer* ptr, int val)
+void setSegs(BitmapLayer* high, BitmapLayer* low, int val)
 {
-	bitmap_layer_set_bitmap(&ptr[0], &bs[val/10].bmp);
-	bitmap_layer_set_bitmap(&ptr[1], &bs[val%10].bmp);
+	bitmap_layer_set_bitmap(high, bs[val/10]);
+	bitmap_layer_set_bitmap(low, bs[val%10]);
 }
 
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent* t)
+void handle_minute_tick(struct tm *time, TimeUnits units_changed)
 {
-	PblTm time;
-	get_time(&time);
-
-	if(!clock_is_24h_style() && time.tm_hour > 12)
+	if(!clock_is_24h_style() && time->tm_hour > 12)
 	{
-		time.tm_hour -= 12;
+		time->tm_hour -= 12;
 	}
-	if(!clock_is_24h_style() && time.tm_hour == 0)
+	if(!clock_is_24h_style() && time->tm_hour == 0)
 	{
-		time.tm_hour = 12;
+		time->tm_hour = 12;
 	}
 
-	setSegs(&bl[0], time.tm_hour);
-	setSegs(&bl[3], time.tm_min);
+	setSegs(bl[0], bl[1], time->tm_hour);
+	setSegs(bl[3], bl[4], time->tm_min);
 
 	#if(DATE)
-		setSegs(&bl[5], time.tm_mon+1);
-		setSegs(&bl[8], time.tm_mday+1);
+		setSegs(bl[5], bl[6], time->tm_mon+1);
+		setSegs(bl[8], bl[9], time->tm_mday+1);
 	#endif
 }
 
-void handle_init(AppContextRef ctx)
+void init(void)
 {
 	int i;
+	static struct tm* now;
+	time_t unix_now;
 
-	resource_init_current_app(&RES_FOR_ANTICHAMBER);
-
-	window_init(&window, "Antichamber");
+	window = window_create();
 
 	for(i = 0; i < 12; i++)
 	{
-		heap_bitmap_init(&bs[i], bsh[i]);
+		bs[i] = gbitmap_create_with_resource(bsh[i]);
 	}
-	bcolon = &bs[10];
-	bdash = &bs[11];
+	bcolon = bs[10];
+	bdash = bs[11];
 
 	#if(DATE)
-		bitmap_layer_init(&bl[0], GRect(0,(168/2)-56,30,51));
-		bitmap_layer_init(&bl[1], GRect(35,(168/2)-56,30,51));
-		bitmap_layer_init(&bl[2], GRect(67,(168/2)-56,9,51));
-		bitmap_layer_init(&bl[3], GRect(79,(168/2)-56,30,51));
-		bitmap_layer_init(&bl[4], GRect(114,(168/2)-56,30,51));
+		bl[0] = bitmap_layer_create(GRect(0,(168/2)-56,30,51));
+		bl[1] = bitmap_layer_create(GRect(35,(168/2)-56,30,51));
+		bl[2] = bitmap_layer_create(GRect(67,(168/2)-56,9,51));
+		bl[3] = bitmap_layer_create(GRect(79,(168/2)-56,30,51));
+		bl[4] = bitmap_layer_create(GRect(114,(168/2)-56,30,51));
 
-		bitmap_layer_init(&bl[5], GRect(0,(168/2)+5,30,51));
-		bitmap_layer_init(&bl[6], GRect(35,(168/2)+5,30,51));
-		bitmap_layer_init(&bl[7], GRect(67,(168/2)+5,9,51));
-		bitmap_layer_init(&bl[8], GRect(79,(168/2)+5,30,51));
-		bitmap_layer_init(&bl[9], GRect(114,(168/2)+5,30,51));
+		bl[5] = bitmap_layer_create(GRect(0,(168/2)+5,30,51));
+		bl[6] = bitmap_layer_create(GRect(35,(168/2)+5,30,51));
+		bl[7] = bitmap_layer_create(GRect(67,(168/2)+5,9,51));
+		bl[8] = bitmap_layer_create(GRect(79,(168/2)+5,30,51));
+		bl[9] = bitmap_layer_create(GRect(114,(168/2)+5,30,51));
 		for(i = 0; i < 10; i++)
-			layer_add_child(&window.layer, &bl[i].layer);
+			layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(bl[i]));
 	#else
-		bitmap_layer_init(&bl[0], GRect(0,(168/2)-25,30,51));
-		bitmap_layer_init(&bl[1], GRect(35,(168/2)-25,30,51));
-		bitmap_layer_init(&bl[2], GRect(67,(168/2)-25,9,51));
-		bitmap_layer_init(&bl[3], GRect(79,(168/2)-25,30,51));
-		bitmap_layer_init(&bl[4], GRect(114,(168/2)-25,30,51));
+		bl[0] = bitmap_layer_create(GRect(0,(168/2)-25,30,51));
+		bl[1] = bitmap_layer_create(GRect(35,(168/2)-25,30,51));
+		bl[2] = bitmap_layer_create(GRect(67,(168/2)-25,9,51));
+		bl[3] = bitmap_layer_create(GRect(79,(168/2)-25,30,51));
+		bl[4] = bitmap_layer_create(GRect(114,(168/2)-25,30,51));
 		for(i = 0; i < 5; i++)
-			layer_add_child(&window.layer, &bl[i].layer);
+			layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(bl[i]));
 	#endif
 
-	window_set_background_color(&window, GColorBlack);
+	window_set_background_color(window, GColorBlack);
 
-	bitmap_layer_set_bitmap(&bl[2], &bcolon->bmp);
+	bitmap_layer_set_bitmap(bl[2], bcolon);
 
 	#if(DATE)
-		bitmap_layer_set_bitmap(&bl[7], &bdash->bmp);
+		bitmap_layer_set_bitmap(bl[7], bdash);
 	#endif
 
-	handle_minute_tick(ctx, NULL);
+	tick_timer_service_subscribe(SECOND_UNIT, &handle_minute_tick);	
 
-	window_stack_push(&window, true);
+	unix_now = time(NULL);
+	now = localtime(&unix_now);
+	handle_minute_tick(now, SECOND_UNIT);
+
+	window_stack_push(window, true);
 }
 
-void handle_deinit(AppContextRef ctx)
+void deinit(void)
 {
 	int i;
 
+	for (i = 0; i < 5; i++)
+		bitmap_layer_destroy(bl[i]);
+
+	#if (DATE)
+		for (i = 5; i < 10; i++)
+			bitmap_layer_destroy(bl[i]);
+	#endif
+
+	window_destroy(window);
+
 	for (i = 0; i < 12; i++)
-	{
-		heap_bitmap_deinit(&bs[i]);
-	}
+		gbitmap_destroy(bs[i]);
 }
 
-void pbl_main(void *params)
+int main(void)
 {
-	PebbleAppHandlers handlers = {
-		.init_handler = &handle_init,
-		.deinit_handler = &handle_deinit,
-		.tick_info = {
-			.tick_handler = &handle_minute_tick,
-			.tick_units = MINUTE_UNIT
-		}
-	};
-	app_event_loop(params, &handlers);
+	init();
+
+	app_event_loop();
+
+	deinit();
 }
